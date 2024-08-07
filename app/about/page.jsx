fa007bcabfb50bffe293c5aeade5f750a844c472
Julius Cinco Cesar <knznsmn@icloud.com>
2024-08-07 07:53:17 +0400

    Fix merge error @ /about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Image from 'next/image';
 import Moment from '@components/api/Moment';
 import './about.modules.css';
@@ -35,40 +34,5 @@ export default function About() {
         </article>
       </section>
     </main>
-=======
-import Moment from '@components/api/Moment';
-import Image from 'next/image';
-import me from '@public/hrafnkell/me.png';
-import './about.modules.css'
-
-// export const metadata = {
-//   title: 'About Me | Julius C. Cesar',
-// };
-const About = () => {
-
- return (
-      <main className="main">
-        <section className="about-left">
-          <article>
-            <h1 className='gradient-bleu' >About</h1>
-            <h3>Hi there!</h3>
-            {/* <p>Good <Moment />. I'm Julius Cinco Cesar. I love coffee and coffee jokes.</p> */}
-          </article>
-
-          <article>
-            <h2>I Volunteer!</h2>
-            <p>I’m on a mission to gain hands-on experience and give back to the community through volunteering. If you’re passionate about creating positive change and have a volunteer opportunity that needs enthusiastic support, I’m all ears! Let’s connect and make a meaningful impact together.</p>
-          </article>
-        </section>
-
-        <section className="about-right">
-          <article>
-            <Image src={me} alt='knznsmn with refueling with a glass of coffee' />
-          </article>
-        </section>
-      </main>
->>>>>>> aa58a3fa29b159c7c825a32c35eb7ae3d1225526
   );
 }
-
-export default About
